3cc24ea74a8760597b1ab21f958b7343426b1135
Dario Pinto <laradiopotin@gmail.com>
2022-08-16 17:25:47 +0200

    change to cohttp

--- lib/request.ml
+++ lib/request.ml
@@ -9,10 +9,10 @@ let request_api config meth endpoint content_type content =
   let url = get_url config endpoint in
   let auth = get_auth_token config in
   let headers = [ ("Authorization", "Basic " ^ auth) ] in
-  EzCurl_lwt.make ~meth ~content_type ~headers ~content url
+  EzCohttp_lwt.post ~meth ~content_type ~headers ~content (URL url)
 
 let request_api_no_body config meth endpoint =
   let url = get_url config endpoint in
   let auth = get_auth_token config in
   let headers = [ ("Authorization", "Basic " ^ auth) ] in
-  EzCurl_lwt.make ~meth ~headers url
+  EzCohttp_lwt.post ~meth ~headers (URL url)
